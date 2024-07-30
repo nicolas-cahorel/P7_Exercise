b21db9048fc2c3a01691ff76e026c0f837e5c516
Nicolas Cahorel <nicolas.cahorel@gmail.com>
2024-07-30 17:51:17 +0200

    step 3.5 : interface SleepDtoDao and UserDtoDao added

---
+++ app/src/main/java/com/openclassrooms/arista/data/dao/SleepDtoDao.kt
@@ -0,0 +1,18 @@
+package com.openclassrooms.arista.data.dao
+
+import androidx.room.Insert
+import androidx.room.Query
+import com.openclassrooms.arista.data.entity.SleepDto
+import kotlinx.coroutines.flow.Flow
+
+interface SleepDtoDao {
+    @Insert
+    suspend fun insertSleep(sleep: SleepDto): Long
+
+    @Query("SELECT * FROM sleep")
+    fun getAllSleepRecords(): Flow<List<SleepDto>>
+
+    @Query("DELETE FROM sleep WHERE id = :id")
+    suspend fun deleteSleepById(id: Long)
+
+}
---
+++ app/src/main/java/com/openclassrooms/arista/data/dao/UserDtoDao.kt
@@ -0,0 +1,19 @@
+package com.openclassrooms.arista.data.dao
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.flow.Flow
+
+interface UserDtoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdateUser(user: UserDto): Long
+
+    @Query("SELECT * FROM user LIMIT 1")
+    fun getUser(): Flow<UserDto?>
+
+    @Query("DELETE FROM user")
+    suspend fun deleteUser()
+
+}
